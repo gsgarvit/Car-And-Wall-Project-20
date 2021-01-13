a71bdb4aa76c5412f192bd4a9a62df502acdc99d
gsgarvit <68574746+gsgarvit@users.noreply.github.com>
2021-01-13 08:05:14 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -12,17 +12,27 @@ function setup() {
 
 function draw() {
   background(0); 
-  if(car.isTouching(wall)){
+  if(wall.x-car.x<(car.width+wall.width)/2){
    car.velocityX=0;
-   deformation=0.5*weight*speed*speed/22500
+  
+   var deformation=0.5* weight* speed* speed/22500;
+
    if(deformation>100){
-    car.setColour="green"
-  }else if(deformation>100||deformation>180){
-    car.setColour="yellow"
-  }else if (deformation<180){
-  car.setColour="red"
-  }
-  }
+
+    car.shapeColor=color(0,255,0)
+
+   }else if(deformation>180 && deformation>100)
+   {
+
+    car.shapeColor=color(230,230,0)
+
+   }else if (deformation<180)
+   {
+
+    car.shapeColor=color(255,0,0)
+
+   }
+}
   
   
   drawSprites();
